8887d9c527c204b4f57e3759b3c6d2bebf9daa67
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-02 11:12:54 +0600

    Start creating corpse handler system

---
+++ Assets/Scripts/Core/Components/CorpseHandler.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Core.Components
+{
+    public class CorpseHandler : MonoBehaviour
+    {
+        [SerializeField] private HealthComponent _healthComponent;
+        [SerializeField] private float destroyedTime = 1f;
+
+        private void OnEnable()
+        {
+            _healthComponent.OnDead.DistinctUntilChanged().Subscribe();
+        }
+
+        private void HandleDeath()
+        {
+            
+        }
+    }
+}

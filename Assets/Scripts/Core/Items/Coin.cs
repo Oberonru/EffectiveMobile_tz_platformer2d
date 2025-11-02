7355de276f0fb97a158d41c98e1b4c530bb62eb2
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-02 15:11:27 +0600

    Create heal and coin scrypt animation

--- Assets/Scripts/Core/Items/Coin.cs
+++ Assets/Scripts/Core/Items/Coin.cs
@@ -7,10 +7,21 @@ namespace Core.Items
     public class Coin : MonoBehaviour, IPickUp
     {
         [SerializeField] private int _nominal;
+        [SerializeField] private float _rotateSpeed = 100f;
 
         public void PickUp(PlayerInstance player)
         {
             Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * _rotateSpeed);
         }
     }
 }
--- Assets/Scripts/Core/Items/Heal.cs
+++ Assets/Scripts/Core/Items/Heal.cs
@@ -7,6 +7,15 @@ namespace Core.Items
     public class Heal : MonoBehaviour, IPickUp
     {
         [SerializeField] private int _healAmount;
+        [SerializeField] private float _amplitude = 0.1f;
+        [SerializeField] private float _speed = 1.0f;
+
+        private float _baseScale;
+
+        private void Start()
+        {
+            _baseScale = transform.localScale.x;
+        }
 
         public void PickUp(PlayerInstance player)
         {
@@ -16,6 +25,17 @@ namespace Core.Items
 
             health.Heal(_healAmount);
             Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            Scaling();
+        }
+
+        private void Scaling()
+        {
+            var scale = 1f + Mathf.Sin(Time.time * _speed) * 0.5f * _amplitude;
+            transform.localScale = new Vector3(scale * _baseScale, scale * _baseScale, transform.localScale.z);
         }
     }
 }

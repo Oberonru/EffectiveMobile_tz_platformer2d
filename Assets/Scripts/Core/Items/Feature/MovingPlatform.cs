63c7ee022c2c593e10bfd4217134a06d2d29185e
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-05 18:03:44 +0600

    Edit llevel_1

---
+++ Assets/Scripts/Core/Items/Feature/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Core.Items.Feature
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField] private float amplitude = 2f; 
+        [SerializeField] private float speed = 2f;
+        private Vector3 _startPos;
+
+        private void Start()
+        {
+            _startPos = transform.position;
+        }
+
+        private void Update()
+        {
+            var offsetY = Mathf.Sin(Time.time * speed) * amplitude;
+            transform.position = _startPos + new Vector3(0, offsetY, 0);
+        }
+    }
+}

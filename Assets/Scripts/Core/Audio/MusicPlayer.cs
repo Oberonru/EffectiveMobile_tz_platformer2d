8cac8a0fe8d8e01cf780edde77bc7d472ce2f228
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-06 18:16:24 +0600

    Refactoring music player

---
+++ Assets/Scripts/Core/Audio/MusicPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Core.Configs.Audio;
+using Core.Handlers;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Audio
+{
+    public enum ClipType
+    {
+        Menu,
+        Level,
+    }
+
+    public class MusicPlayer : MonoBehaviour
+    {
+        [Inject] private IAudioHandler _handler;
+        [Inject] private AudioClipsConfig _config;
+
+        [SerializeField] private ClipType _clipType = ClipType.Menu;
+
+        private void Start()
+        {
+            SwitchMusic();
+        }
+
+        private void OnDisable()
+        {
+            _handler.StopMusic();
+        }
+
+        private void SwitchMusic()
+        {
+            if (_clipType == ClipType.Menu)
+            {
+                _handler.PlayMusic(_config.MainMenuClip, true);
+            }
+
+            if (_clipType == ClipType.Level)
+            {
+                _handler.PlayMusic(_config.LevelMusicClip, true);
+            }
+        }
+    }
+}

d51e8085ae8ad6fad2f4c7e98f95791ed9bd457a
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-06 17:29:58 +0600

    Creating main menu

---
+++ Assets/Scripts/Core/UI/Screens/MainMenuScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.UI.Screens
+{
+    public class MainMenuScreen : UIScreen
+    {
+        [SerializeField] private Button _play;
+    }
+}

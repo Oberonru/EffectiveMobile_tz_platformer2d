4f71aadccf10a9bccaeb3ad25046261a2fbe3d15
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-06 20:34:06 +0600

    Edit menu -> level transition

--- Assets/Scripts/Core/UI/Screens/MainMenuScreen.cs
+++ Assets/Scripts/Core/UI/Screens/MainMenuScreen.cs
@@ -1,10 +1,48 @@
-﻿using UnityEngine;
+﻿using Core.Model;
+using Core.UI.Screens.Data;
+using Infrastructure.Services;
+using UniRx;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace Core.UI.Screens
 {
     public class MainMenuScreen : UIScreen
     {
+        [Inject] private IScreenHandler _handler;
+        [Inject] private SceneLoaderService _loader;
+        
         [SerializeField] private Button _play;
+        [SerializeField] private Button _settings;
+        [SerializeField] private Button _exit;
+        
+        private void OnEnable()
+        {
+            _play.onClick.
+                AsObservable().
+                Subscribe(_ => _loader.LoadScene(SceneName.Level1.ToString())).
+                AddTo(this);
+
+            _settings.onClick.
+                AsObservable().
+                Subscribe(_ => _handler.SetScreen(ScreenType.Settings)).
+                AddTo(this);
+
+            _exit.onClick.
+                AsObservable().
+                Subscribe(_ => Exit()).
+                AddTo(this);
+        }
+
+        private void Exit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 }
---
+++ Assets/Scripts/Core/UI/Screens/SettingsScreen.cs
@@ -0,0 +1,22 @@
+﻿using Core.Model;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Core.UI.Screens
+{
+    public class SettingsScreen : UIScreen
+    {
+        [Inject] private IScreenHandler _handler;
+        [SerializeField] private Button _back;
+
+        private void OnEnable()
+        {
+            _back.onClick.
+                AsObservable().
+                Subscribe(_ => _handler.SetScreen(ScreenType.MainMenu)).
+                AddTo(this);
+        }
+    }
+}
